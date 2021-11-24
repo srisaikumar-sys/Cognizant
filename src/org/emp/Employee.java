9f02ffdee86edfe791e12aa68246d23af3f42eb3
srisai <srisai.@gmail.com>
2021-11-24 22:58:54 +0530

    Pushing to Master

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+import java.util.Scanner;
+
+public class Employee {
+	private static Scanner s;
+
+	public static void main(String[] args) {
+		s = new Scanner(System.in);
+		System.out.println("Enter the age:");
+		int age=s.nextInt();
+		System.out.println("Having Voter ID:");
+		boolean ID=s.nextBoolean();
+		
+
+		if(age>18) {
+			if(ID==true){
+				
+				System.out.println("You are Eligible for Vote");	
+			}else
+			{
+				System.out.println("Please bring Voter ID");
+			}
+			
+			
+		}else
+		{
+		System.out.println("Not eligible to vote");	
+		}
+	}
+	
+	
+	
+		
+	}
+
